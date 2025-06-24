d2cdc6a830c48661b57e7ca0993ecf46c9520c2e
Nishant Kumar Chaubey <chaubeynishant2@gmail.com>
2025-06-25 00:45:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const petRoutes = require('./routes/petRoutes');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
 app.use(express.json());
 app.use('/api/pets', petRoutes);
 
